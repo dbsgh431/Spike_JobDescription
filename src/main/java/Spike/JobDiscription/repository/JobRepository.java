c2abb6aa6148d2520959783117fa705fa1a3cf1b
yoonho <qufdbsgh431@gmail.com>
2023-11-26 21:15:57 +0900

    refactor : 레포지토리 인터페이스, 구현체 분리 및 스프링에 독립적인 jobService 테스트 구현

--- src/main/java/Spike/JobDiscription/repository/JobRepository.java
+++ src/main/java/Spike/JobDiscription/repository/JobRepository.java
@@ -1,19 +1,23 @@
 package Spike.JobDiscription.repository;
 
 import Spike.JobDiscription.entity.Job;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
+public interface JobRepository {
 
-public interface JobRepository extends CrudRepository<Job, Long> {
-
-    @Override
     List<Job> findAll();
 
-    @Query(value = "SELECT * FROM job WHERE user_id = :userId", nativeQuery = true)
-    List<Job> findByUserId(@Param("userId") Long userId);
+    List<Job> findByUserId(Long userId);
+
+    Job save(Job job);
+
+    Optional<Job> findById(Long id);
+
+    void delete(Job job);
+
+
+    void clear();
 
 }
---
+++ src/main/java/Spike/JobDiscription/repository/JobRepositoryImplJpa.java
@@ -0,0 +1,29 @@
+package Spike.JobDiscription.repository;
+
+import Spike.JobDiscription.entity.Job;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
+
+
+public interface JobRepositoryImplJpa extends CrudRepository<Job, Long>, JobRepository {
+
+    @Override
+    Job save(Job job);
+
+    @Override
+    Optional<Job> findById(Long Id);
+
+    @Override
+    List<Job> findAll();
+
+    @Override
+    void delete(Job entity);
+
+    @Query(value = "SELECT * FROM job WHERE user_id = :userId", nativeQuery = true)
+    List<Job> findByUserId(@Param("userId") Long userId);
+
+}
